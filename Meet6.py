12b927961cada10ecf654610c3fe2cb1b129585a
yogi-saputr <145946382+yogi-saputr@users.noreply.github.com>
2024-05-14 12:18:36 +0800

    Add files via upload

---
+++ Meet6.py
@@ -0,0 +1,38 @@
+# perbandingan >, <, <=, >=, ==, !=, is, isnot
+
+x = 3
+y = 4
+
+# Lebih besar >
+hasil = x > y
+print(x, ">", y, "=", hasil)
+
+# Lebih Kecil <
+hasil = x < y
+print(x, "<", y, "=", hasil)
+
+#Lebih besar >=
+hasil = x >= y
+print(x, ">=", y, "=", hasil)
+print(x, ">=", 3, "=", hasil)
+
+# Lebih kecil <=
+hasil = x <= y
+print(x, "<=", y, "=", hasil)
+print(x, "<=", 3, "=", hasil)
+
+# sama dengan ==
+hasil = x == 9
+print(x, "==", 9, "=", hasil)
+
+# tidak sama dengan
+hasil = x != 9
+print(x, "!=", 9, "=", hasil)
+
+# is (adalah)
+hasil = x is 9
+print(x, "is", 9, "=", hasil)
+
+# isnot 
+hasil = x is not 9
+print(x, "is not", 9, "=", hasil)
---
+++ meet4&5.py
@@ -0,0 +1,40 @@
+# ariitmatika 
+
+x = 9 
+y = 3
+
+# penjumlahan +
+hasil = x + y
+print(x, "+", y, "=", hasil)
+
+# pengurangan -
+hasil = x - y
+print(x, "-", y, "=", hasil)
+
+# perkalian *
+hasil = x * y
+print(x, "*", y, "=", hasil)
+
+#pembagian /
+hasil = x / y
+print(x, "/", y, "=", hasil)
+
+# modulus %
+hasil = x % y
+print(x, "%", y, "=", hasil)
+
+# perpangkatan **
+hasil = x ** y
+print(x, "**", y, "=", hasil)
+
+# floor division //
+hasil = x // y
+print(x, "//", y, "=", hasil)
+
+#skala prioritas
+x = 3
+y = 4 
+z = 5
+hasil = (x ** y * (z + x) / y - z % x // y)
+print(hasil)
+print(x, "**", y, "*", z, "+", x, "/", y, "-", z, "%", x, "//", y, "=", hasil)
